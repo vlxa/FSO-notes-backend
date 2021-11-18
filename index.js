91a117974895907b4ecc679172c4a356ff60f638
vlxa <vit.lexa@gmail.com>
2021-11-18 19:10:00 +0100

    incorrect logic in backend delete fixed

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/api/notes/:id', (request, response) => {
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = +request.params.id
-  notes = notes.filter((note) => note.id === id)
+  notes = notes.filter((note) => note.id !== id)
 
   response.status(204).end()
 })
