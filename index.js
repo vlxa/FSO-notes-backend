47b74872b4f35e0fa7d5c981bad2cb9fb7ee1a5d
vlxa <vit.lexa@gmail.com>
2021-11-23 17:33:44 +0100

    changes for deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
   {
@@ -85,7 +87,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.port || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
