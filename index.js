311063f3449cea30e08e99c2de2c3c4cfeb0a661
vlxa <vit.lexa@gmail.com>
2021-12-01 15:25:16 +0100

    deploy with mongo

--- index.js
+++ index.js
@@ -13,33 +13,33 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then((notes) => {
+  Note.find({}).then(notes => {
     response.json(notes)
   })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
-    .then((note) => {
+    .then(note => {
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then((result) => response.status(204).end())
-    .catch((error) => next(error))
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 const generateId = () => {
   const maxId =
     notes.length > 0 //force-wrap
-      ? Math.max(...notes.map((n) => n.id))
+      ? Math.max(...notes.map(n => n.id))
       : 0
   return maxId + 1
 }
@@ -59,7 +59,7 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then((savedNote) => {
+  note.save().then(savedNote => {
     response.json(savedNote)
   })
 })
@@ -72,14 +72,14 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
-    .then((updatedNote) => {
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
       response.json(updatedNote)
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
@@ -89,7 +89,7 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.name)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
   next(error)
