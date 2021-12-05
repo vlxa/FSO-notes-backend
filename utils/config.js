135902300133bfd35849695f75d1381e293656fe
vlxa <vit.lexa@gmail.com>
2021-12-05 23:15:51 +0100

    database tests
    setting up database tests with jest and supertest

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-// telling eslint process is defined elsewhere
-/* global process */
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
