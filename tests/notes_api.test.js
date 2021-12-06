f428863a90163945e46a0ea0bc2bf6bf5bc3b133
vlxa <vit.lexa@gmail.com>
2021-12-06 14:17:07 +0100

    populate test database
    now populates test database before test are run

--- tests/notes_api.test.js
+++ tests/notes_api.test.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+]
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -14,13 +35,14 @@ test('notes are returned as json', async () => {
 test('there are two notes', async () => {
   const response = await api.get('/api/notes')
 
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 
 test('the first note is about HTTP methods', async () => {
   const response = await api.get('/api/notes')
 
-  expect(response.body[0].content).toBe('HTML is easy')
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain('Browser can execute only Javascript')
 })
 
 afterAll(() => {
