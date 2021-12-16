2433fb9961d940ab8b7500b76861a7316a641062
vlxa <vit.lexa@gmail.com>
2021-12-16 14:26:35 +0100

    user tests

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,33 +1,40 @@
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const initialNotes = [
   {
     content: 'HTML is easy',
     date: new Date(),
-    important: false
+    important: false,
   },
   {
     content: 'Browser can execute only Javascript',
     date: new Date(),
-    important: true
-  }
+    important: true,
+  },
 ]
 
 const nonExistingId = async () => {
   const note = new Note({ content: 'willremovethissoon', date: new Date() })
   await note.save()
   await note.remove()
-
+  /* eslint-disable no-underscore-dangle */
   return note._id.toString()
 }
 
 const notesInDb = async () => {
   const notes = await Note.find({})
-  return notes.map(note => note.toJSON())
+  return notes.map((note) => note.toJSON())
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((u) => u.toJSON())
 }
 
 module.exports = {
   initialNotes,
   nonExistingId,
   notesInDb,
+  usersInDb,
 }
