e53e24bd787f3d4b9713c332103f0817723fdc70
vlxa <vit.lexa@gmail.com>
2021-12-17 20:17:11 +0100

    fixed error in user api test

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -7,16 +7,16 @@ const User = require('../models/user')
 
 const api = supertest(app)
 
+beforeEach(async () => {
+  await User.deleteMany({})
+
+  const passwordHash = await bcrypt.hash('sekret', 10)
+  const user = new User({ username: 'root', passwordHash })
+
+  await user.save()
+})
+
 describe('when there is initially one user in db', () => {
-  beforeEach(async () => {
-    await User.deleteMany({})
-
-    const passwordHash = await bcrypt.hash('sekret', 10)
-    const user = new User({ username: 'root', passwordHash })
-
-    await user.save()
-  })
-
   test('creation succeeds with a fresh username', async () => {
     const usersAtStart = await helper.usersInDb()
 
@@ -35,7 +35,7 @@ describe('when there is initially one user in db', () => {
     const usersAtEnd = await helper.usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
 
-    const usernames = usersAtEnd.map((u) => u.username)
+    const usernames = usersAtEnd.map(u => u.username)
     expect(usernames).toContain(newUser.username)
   })
 
